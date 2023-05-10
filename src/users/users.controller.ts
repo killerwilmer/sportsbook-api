7c34b8ecbd7e6d5f3ea58bf5cd20fda81e093038
Wilmer Arteaga <killerwilmer@gmail.com>
2023-05-10 14:46:30 -0500

    get all users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,19 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
+  @Get()
+  getUsers(): Promise<User[]> {
+    return this.usersService.getUsers();
+  }
+
   @Post()
-  createUser(@Body() newUser: CreateUserDto) {
+  createUser(@Body() newUser: CreateUserDto): Promise<User> {
     return this.usersService.createUser(newUser);
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,8 +9,13 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private userRepository: Repository<User>,
   ) {}
+
   createUser(user: CreateUserDto) {
     const newUser = this.userRepository.create(user);
     return this.userRepository.save(newUser);
+  }
+
+  getUsers() {
+    return this.userRepository.find();
   }
 }
