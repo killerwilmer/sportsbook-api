98addab51db4ac4a0c4ed7b09ea0848d82751c55
Wilmer Arteaga <killerwilmer@gmail.com>
2023-05-11 20:16:43 -0500

    change port

--- src/app.module.ts
+++ src/app.module.ts
@@ -25,7 +25,7 @@ import config from './config';
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+      synchronize: false,
     }),
     UsersModule,
     AuthModule,
